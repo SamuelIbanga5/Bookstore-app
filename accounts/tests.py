326036227d7d790a74947954dfd76e39798e5e7b
Samuel Ibanga <ibangajnr27@gmail.com>
2022-11-25 01:08:41 +0100

    Bookstore app

---
+++ accounts/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="Ibangajnr", email="ibangajnr27@gmail.com", password="software"
+        )
+        self.assertEqual(user.username, "Ibangajnr")
+        self.assertEqual(user.email, "ibangajnr27@gmail.com")
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username="superadmin", email="superadmin@gmail.com", password="superadmin"
+        )
+        self.assertEqual(admin_user.username, "superadmin")
+        self.assertEqual(admin_user.email, "superadmin@gmail.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
