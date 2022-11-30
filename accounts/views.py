866a0eea064c0e4bf0250472cf456e7d82a363fe
Samuel Ibanga <ibangajnr27@gmail.com>
2022-11-30 12:51:19 +0100

    Deleted files

--- accounts/views.py
+++
@@ -1,8 +0,0 @@
-from django.urls import reverse_lazy
-from .forms import CustomUserCreationForm
-from django.views import generic
-
-class SignupPageView(generic.CreateView):
-    form_class = CustomUserCreationForm
-    success_url = reverse_lazy("login")
-    template_name = "registration/signup.html"
